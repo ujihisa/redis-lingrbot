9b026744be570432d279ad828341ec5ce59b19a3
ujihisa <ujihisa@gmail.com>
2013-04-28 16:45:31 -0700

    truncate

--- tester.rb
+++ tester.rb
@@ -4,8 +4,11 @@ s = TCPSocket.open('localhost', 6379)
 s.write "#{ARGV.shift}\r\n"
 begin
   timeout(5) do
-    message, _ = s.recvfrom(999)
-    p message
+    message, _ = s.recvfrom(1024*10)
+    if message.size > 1000
+      "#{message[0...800]}... (total #{message.size} chars)"
+    else
+      message
   end
 rescue Timeout::Error
   "Timed out!"
