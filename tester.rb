8cacfa507e6f4c87fef45daa3a2ca330c7c6a0d0
ujihisa <ujihisa@gmail.com>
2013-04-28 16:43:34 -0700

    with timeout

--- tester.rb
+++ tester.rb
@@ -1,9 +1,12 @@
+require 'timeout'
 require 'socket'
 s = TCPSocket.open('localhost', 6379)
 s.write "#{ARGV.shift}\r\n"
-p 1
-p s.gets
-p s.gets
-p s.gets
-p s.gets
-p 3
+begin
+  timeout(5) do
+    message, client_address = s.recvfrom(999)
+    p [message, client_address]
+  end
+rescue Timeout::Error
+  "Timed out!"
+end
