7d80d7b3b5dbdaf0b0fbf2c392866bbfdc4f78f7
ujihisa <ujihisa@gmail.com>
2013-04-28 16:03:36 -0700

    lingr bot for ruby template

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+
+get '/' do
+  {RUBY_DESCRIPTION: RUBY_DESCRIPTION}.inspect
+end
+
+post '/' do
+  begin
+    JSON.parse(request.body.string)['events'].map {|event|
+      msg = event['message']
+      next unless %w[computer_science vim].include? msg['room']
+      p msg['text']
+      ''
+    }.join
+  rescue => e
+    p e
+    ''
+  end
+end
+
